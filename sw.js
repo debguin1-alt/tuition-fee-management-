ce188f06f0a585a7d10d747546583ca413090817
debguin1-alt <debguin1@gmail.com>
2026-01-27 12:47:33 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -21,5 +21,6 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => response || fetch(event.request))
+      .catch(() => caches.match('/index.html'))
   );
 });
